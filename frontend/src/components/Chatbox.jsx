e7313ced5acaea2e82656788ec47c714c4dbad42
ParagGhatage <phghatage1@gmail.com>
2024-10-19 18:05:48 +0530

    removing api route and integrating call in frontend

--- frontend/src/components/Chatbox.jsx
+++ frontend/src/components/Chatbox.jsx
@@ -4,6 +4,7 @@ import ChatInput from './ChatInput';
 import Message from './Message';
 import { Plan } from './plan/page';
 import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
 
 
 const Chatbox = () => {
@@ -17,16 +18,23 @@ const Chatbox = () => {
   const handleSendMessage = async (query) => {
     setMessages("Loading ...")
     setref("")
-    // Fetch response from the chatbot API
-    const response = await fetch('/api/chat/', {
-      method: 'POST',
-      body: JSON.stringify({ query }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    // Declare the `returned` variable
+    let dataa;
 
-    const dataa = await response.json();
+     // Mocked chatbot response function for now
+    const response = await axios.post('https://briefly-80161481785.us-central1.run.app/search',{
+    
+       "query":`${query}` },
+      {headers:{
+        'Content-Type': 'application/json',
+      }},
+    );
+    console.log(response)
+    console.log(response.data)
+    if(response.data.result){
+    dataa=`Query: "${query}"\n Response:${response.data.result.document}`;
+
+    // const dataa = await returned.json();
     console.log(dataa)
     // Assuming `Plan` is an API call that returns an answer object
     const r = await Plan(query,dataa);
@@ -34,6 +42,23 @@ const Chatbox = () => {
     //console.log(r.Answer )
     setMessages(r.Answer)
     setref(r.RelevantLaws)
+    }
+    else
+    {
+      console.log(returned.data.message)
+      dataa=`You asked: "${query}"\n Response:${response.data.message}`
+      // const dataa = await returned.json();
+    console.log(dataa)
+    // Assuming `Plan` is an API call that returns an answer object
+    const r = await Plan(query,dataa);
+    console.log(r )
+    //console.log(r.Answer )
+    setMessages(r.Answer)
+    setref(r.RelevantLaws)
+    }
+  
+  
+
     
         
   };
